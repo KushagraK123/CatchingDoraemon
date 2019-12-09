40d84acbeb0d383b7e0e344765d9f7037b81d3fa
_kushagra_ <kushagrakasliwal47@gmail.com>
2019-12-09 12:56:29 +0530

    added new background

--- app/src/main/java/com/empyrealgames/catchingdoraemon/MainActivity.kt
+++ app/src/main/java/com/empyrealgames/catchingdoraemon/MainActivity.kt
@@ -2,6 +2,7 @@ package com.empyrealgames.catchingdoraemon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.View
 import androidx.gridlayout.widget.GridLayout
 import kotlinx.android.synthetic.main.activity_main.*
@@ -10,15 +11,19 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private var score: Int = 0
+    private var time: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 
 
     fun increaseScore(view: View){
         score++
-        scoreText.setText("Score : " + score)
+        scoreText.setText("Score : $score" )
         val p: GridLayout.LayoutParams = GridLayout.LayoutParams(doraemon.layoutParams)
         val r:Random = Random()
         p.columnSpec = GridLayout.spec(r.nextInt(3));
