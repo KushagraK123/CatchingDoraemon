6032569ff5e9a1b35c7a4451665cdaccb9ee2795
_kushagra_ <kushagrakasliwal47@gmail.com>
2019-12-09 10:21:17 +0530

    added grid layout

--- app/src/main/java/com/empyrealgames/catchingdoraemon/MainActivity.kt
+++ app/src/main/java/com/empyrealgames/catchingdoraemon/MainActivity.kt
@@ -2,11 +2,23 @@ package com.empyrealgames.catchingdoraemon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var score: Int = 0
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
+    fun increaseScore(view: View){
+        score++
+        scoreText.setText(score.toString())
+    }
+
 }
