67937e3309ca5dfbb023592d7c720049c9f5744a
_kushagra_ <kushagrakasliwal47@gmail.com>
2019-12-10 11:10:32 +0530

    created start activity

---
+++ app/src/main/java/com/empyrealgames/catchingdoraemon/StartActivity.kt
@@ -0,0 +1,24 @@
+package com.empyrealgames.catchingdoraemon
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_start.*
+
+class StartActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+    }
+
+    fun startGame(view: View){
+
+        val i = Intent(applicationContext, MainActivity::class.java)
+        startActivity(i)
+
+    }
+}
